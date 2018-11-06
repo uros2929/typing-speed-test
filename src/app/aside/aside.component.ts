017091201424c3f2d1e8565501bc17d2270dfe8d
uros2929 <marinko29@live.com>
2018-11-06 04:21:16 +0100

    skoro pa gotovo :)

--- src/app/aside/aside.component.ts
+++ src/app/aside/aside.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.scss']
 })
 export class AsideComponent implements OnInit {
-
+  results=Object.keys(localStorage.getItem('results') !== null ? JSON.parse(localStorage.getItem('results')) : {});
+  name;
+  words;
+  totalTime;
+  speed;
+  mistakes;
   constructor() { }
 
   ngOnInit() {
+ 
+  }
+  showResults(event){
+    document.getElementById('showResults').style.display="block";
+  let fromLocal=JSON.parse(localStorage.getItem('results'))[event.target.id];
+  this.name=fromLocal.name;
+  this.words=fromLocal.wordsNum;
+  this.totalTime=fromLocal.totalTime;
+  this.speed=fromLocal.speed;
+  this.mistakes=fromLocal.mistakes;
+  }
+  exitShowResults(){
+    document.getElementById('showResults').style.display="none";
   }
 
 }
